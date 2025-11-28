f9ca18fbf9b19a902750b838dadd52473e9b1447
Nicolas DESBAN <nicolas.desban@etudiant.univ-reims.fr>
2025-11-29 00:45:47 +0100

    feat(entity): generate Questionnaire entity and repository

---
+++ api/src/Entity/Questionnaire.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionnaireRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuestionnaireRepository::class)]
+class Questionnaire
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\ManyToOne]
+    private ?Question $startQuestion = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getStartQuestion(): ?Question
+    {
+        return $this->startQuestion;
+    }
+
+    public function setStartQuestion(?Question $startQuestion): static
+    {
+        $this->startQuestion = $startQuestion;
+
+        return $this;
+    }
+}

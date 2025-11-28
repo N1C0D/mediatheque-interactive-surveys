4da9919333d9eb07dcfc696e706cc5cd6dcf4049
Nicolas DESBAN <nicolas.desban@etudiant.univ-reims.fr>
2025-11-29 00:42:12 +0100

    feat(entity): generate Choice entity and repository

---
+++ api/src/Entity/Choice.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChoiceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChoiceRepository::class)]
+class Choice
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $label = null;
+
+    #[ORM\ManyToOne(inversedBy: 'choices')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Question $question = null;
+
+    #[ORM\ManyToOne]
+    private ?Question $targetQuestion = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getQuestion(): ?Question
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(?Question $question): static
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function getTargetQuestion(): ?Question
+    {
+        return $this->targetQuestion;
+    }
+
+    public function setTargetQuestion(?Question $targetQuestion): static
+    {
+        $this->targetQuestion = $targetQuestion;
+
+        return $this;
+    }
+}
--- api/src/Entity/Question.php
+++ api/src/Entity/Question.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\QuestionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -22,6 +24,17 @@ class Question
 
     #[ORM\Column(length: 50, nullable: true)]
     private ?string $mediaType = null;
+
+    /**
+     * @var Collection<int, Choice>
+     */
+    #[ORM\OneToMany(targetEntity: Choice::class, mappedBy: 'question', orphanRemoval: true)]
+    private Collection $choices;
+
+    public function __construct()
+    {
+        $this->choices = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -60,6 +73,36 @@ class Question
     public function setMediaType(?string $mediaType): static
     {
         $this->mediaType = $mediaType;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Choice>
+     */
+    public function getChoices(): Collection
+    {
+        return $this->choices;
+    }
+
+    public function addChoice(Choice $choice): static
+    {
+        if (!$this->choices->contains($choice)) {
+            $this->choices->add($choice);
+            $choice->setQuestion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeChoice(Choice $choice): static
+    {
+        if ($this->choices->removeElement($choice)) {
+            // set the owning side to null (unless already changed)
+            if ($choice->getQuestion() === $this) {
+                $choice->setQuestion(null);
+            }
+        }
 
         return $this;
     }

e60b57373141807dc756423ea726b4f71a02c6f5
Nicolas DESBAN <nicolas.desban@etudiant.univ-reims.fr>
2025-11-30 03:50:07 +0100

    feat(api): enhance Choice entity with API

--- api/src/Entity/Choice.php
+++ api/src/Entity/Choice.php
@@ -2,26 +2,50 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\ChoiceRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ChoiceRepository::class)]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Get(),
+        new Post(security: "is_granted('ROLE_ADMIN')"),
+        new Put(security: "is_granted('ROLE_ADMIN')"),
+        new Patch(security: "is_granted('ROLE_ADMIN')"),
+        new Delete(security: "is_granted('ROLE_ADMIN')"),
+    ],
+    normalizationContext: ['groups' => ['choice:read']],
+    denormalizationContext: ['groups' => ['choice:write']],
+)]
 class Choice
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['choice:read', 'question:read', 'questionnaire:read', 'participation:read', 'participationAnswer:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['choice:read', 'choice:write', 'question:read', 'questionnaire:read', 'participation:read', 'participationAnswer:read'])]
     private ?string $label = null;
 
     #[ORM\ManyToOne(inversedBy: 'choices')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['choice:read', 'choice:write'])]
     private ?Question $question = null;
 
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(onDelete: 'SET NULL')]
+    #[Groups(['choice:read', 'choice:write', 'question:read', 'questionnaire:read', 'participation:read'])]
     private ?Question $targetQuestion = null;
 
     public function getId(): ?int

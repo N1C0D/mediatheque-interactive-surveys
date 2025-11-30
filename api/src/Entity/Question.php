2f96b05dc0d3a47d29862779365ed0509bec2774
Nicolas DESBAN <nicolas.desban@etudiant.univ-reims.fr>
2025-11-30 04:11:25 +0100

    feat(api): add filters to Question entity

--- api/src/Entity/Question.php
+++ api/src/Entity/Question.php
@@ -2,6 +2,9 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
@@ -29,6 +32,8 @@ use Symfony\Component\Serializer\Annotation\Groups;
     normalizationContext: ['groups' => ['question:read']],
     denormalizationContext: ['groups' => ['question:write']],
 )]
+#[ApiFilter(SearchFilter::class, properties: ['content' => 'partial', 'questionnaire' => 'exact', 'mediaType' => 'exact'])]
+#[ApiFilter(OrderFilter::class, properties: ['id'], arguments: ['orderParameterName' => 'order'])]
 class Question
 {
     #[ORM\Id]

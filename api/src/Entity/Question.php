05a5169fd7e77be7ce38eb180ad17fefe4ff2ab9
Nicolas DESBAN <nicolas.desban@etudiant.univ-reims.fr>
2025-11-30 03:44:12 +0100

    feat(api): enhance Question entity with API

--- api/src/Entity/Question.php
+++ api/src/Entity/Question.php
@@ -2,37 +2,63 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\QuestionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: QuestionRepository::class)]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Get(),
+        new Post(security: "is_granted('ROLE_ADMIN')"),
+        new Put(security: "is_granted('ROLE_ADMIN')"),
+        new Patch(security: "is_granted('ROLE_ADMIN')"),
+        new Delete(security: "is_granted('ROLE_ADMIN')"),
+    ],
+    normalizationContext: ['groups' => ['question:read']],
+    denormalizationContext: ['groups' => ['question:write']],
+)]
 class Question
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['question:read', 'questionnaire:read', 'choice:read', 'participation:read'])]
     private ?int $id = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['question:read', 'question:write', 'questionnaire:read', 'participation:read'])]
     private ?string $content = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['question:read', 'question:write', 'questionnaire:read', 'participation:read'])]
     private ?string $mediaFilename = null;
 
     #[ORM\Column(length: 50, nullable: true)]
+    #[Groups(['question:read', 'question:write', 'questionnaire:read', 'participation:read'])]
     private ?string $mediaType = null;
 
     /**
      * @var Collection<int, Choice>
      */
     #[ORM\OneToMany(targetEntity: Choice::class, mappedBy: 'question', cascade: ['persist'], orphanRemoval: true)]
+    #[Groups(['question:read', 'questionnaire:read', 'participation:read'])]
     private Collection $choices;
 
     #[ORM\ManyToOne(inversedBy: 'questions')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['question:read', 'question:write'])]
     private ?Questionnaire $questionnaire = null;
 
     public function __construct()

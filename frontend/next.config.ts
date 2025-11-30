cd4555d341eb6661b779bb2a4c69540c59c046d7
Nicolas DESBAN <nicolas.desban@etudiant.univ-reims.fr>
2025-11-30 04:54:56 +0100

    feat(config): update Next.js configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'localhost',
+                port: '',
+                pathname: '/uploads/**',
+            },
+        ],
+    },
+    webpack: (config) => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        }
+        return config
+    },
 };
 
 export default nextConfig;
